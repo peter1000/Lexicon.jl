6a12f712531ee7518ab7c77c4ccf5438dd91b3c7
peter1000 <https://github.com/peter1000>
2015-05-08 12:44:31 -0300

    Adds ThirdParty.jl file

--- src/Lexicon.jl
+++ src/Lexicon.jl
@@ -1,15 +1,32 @@
 module Lexicon
 
-using
+"""
+!!author(Michael Hatherly)
+!!copyright(Michael Hatherly and other contributors)
+!!license([MIT](https://github.com/MichaelHatherly/Lexicon.jl/blob/master/LICENSE.md))
 
-    Docile,
-    Docile.Interface
+*Lexicon* is a [Julia](http://www.julialang.org) package documentation generator
+and viewer.
+
+It provides access to the documentation created by the `@doc` macro from
+[*Docile*](https://github.com/MichaelHatherly/Docile.jl). *Lexicon* allows
+querying of package documentation from the Julia REPL and building standalone
+documentation that can be hosted on GitHub Pages or
+[Read the Docs](https://readthedocs.org/).
+
+!!section(Lexicon.md/Lexicon/1)
+"""
+Lexicon
+
+using Docile, Docile.Interface
 
 
-include("Utilities.jl")                                 # Code useful across submodules.
+include("Utilities.jl")                         # Code useful across submodules.
 
-isfile("../.docile") && include("../.docile")           # Central package documentation configuration.
+isfile("../.docile") && include("../.docile")   # Central package documentation configuration.
 
-include("Render/Render.jl")                             # Render-dispatch for output of docstrings.
+include("Render/Render.jl")                     # Render-dispatch for output of docstrings.
+
+include("ThirdParty/ThirdParty.jl")             # Code related to ease usage of 3rd-party programs.
 
 end 
---
+++ src/ThirdParty/ThirdParty.jl
@@ -0,0 +1,12 @@
+module ThirdParty
+
+"""
+This module contains code to ease usage of 3rd-party programs such as the static site generator
+[MkDocs](https://github.com/mkdocs/mkdocs/).
+
+!!section(ThirdParty.md/ThirdParty/1)
+"""
+ThirdParty
+
+
+end
