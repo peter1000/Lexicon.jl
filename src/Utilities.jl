5bba6d2b3d5f2d78bbee447d0afd23e785dd2e03
peter1000 <https://github.com/peter1000>
2015-05-31 05:57:01 -0300

    Adds UTF8String convert

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -7,4 +7,9 @@ Utilities
 
 using Compat
 
+convert(::Type{UTF8String}, sym::Symbol)    = utf8(string(sym))
+convert(::Type{UTF8String}, s::ASCIIString) = utf8(s)
+
+utf8checked!(d::Dict, k) = isa(d[k], UTF8String) || (d[k] = convert(UTF8String, d[k]))
+
 end
