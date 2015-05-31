94248be18de261b95fcd9206de6f98553a598f41
peter1000 <https://github.com/peter1000>
2015-05-31 10:38:25 -0300

    utf8checked! return value of key

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -10,6 +10,8 @@ using Compat
 convert(::Type{UTF8String}, sym::Symbol)    = utf8(string(sym))
 convert(::Type{UTF8String}, s::ASCIIString) = utf8(s)
 
-utf8checked!(d::Dict, k) = isa(d[k], UTF8String) || (d[k] = convert(UTF8String, d[k]))
+function utf8checked!(d::Dict{Symbol, Any}, k)
+    isa(d[k], UTF8String) ? d[k] : (d[k] = convert(UTF8String, d[k]); d[k])
+end
 
 end
--- test/Utilities/facts.jl
+++ test/Utilities/facts.jl
@@ -10,10 +10,11 @@ facts("Utilities.") do
         @fact isa(convert(UTF8String, :sym), UTF8String)    => true
         @fact isa(convert(UTF8String, "ascii"), UTF8String) => true
 
-        d = @compat(Dict(:A => :sym, :B => "ascii", :C => "unicode 出名字"))
-        utf8checked!(d, :A)
-        utf8checked!(d, :B)
-        utf8checked!(d, :C)
+        d = @compat(Dict{Symbol, Any}(:A => :sym, :B => "ascii", :C => "unicode 出名字"))
+
+        @fact utf8checked!(d, :A) => "sym"
+        @fact utf8checked!(d, :B) => "ascii"
+        @fact utf8checked!(d, :C) => "unicode 出名字"
 
         @fact isa(d[:A], UTF8String) => true
         @fact isa(d[:B], UTF8String) => true
